9cd402be0e61cc2185ae141bad69d350b3160f2c
Tracey <jritimecard@gmail.com>
2020-11-29 14:13:30 +0000

    Create StudentMemento Class
    Declare the fields present in the StudentOriginator Class. In this instance only using the student GPA as this is the only value that will change over time

---
+++ src/StudentMemento.java
@@ -0,0 +1,12 @@
+
+public class StudentMemento {
+    private float gpa;
+
+    public StudentMemento(float gpa){
+        this.gpa = gpa;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+}
